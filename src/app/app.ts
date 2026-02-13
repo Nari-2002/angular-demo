e718f96125f8ceed2cccbc4f5cbc610ef5e4512a
narendra_kamepalli <nari73092@gmail.com>
2026-02-13 18:02:09 +0530

    databinding

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,18 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Demo } from '../components/demo/demo';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [Demo],
+  imports: [Demo,FormsModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('angular-demo');
+  name:string="narendra"
+  title = 'angular-demo';
+  sayHi(){
+    alert("hello")
+  }
 }
