40aed0f1ba391e848c2e7e160667fdd71315852a
narendra_kamepalli <nari73092@gmail.com>
2026-02-14 17:43:43 +0530

    ngClass, ngStyle

--- src/components/demo/demo.ts
+++ src/components/demo/demo.ts
@@ -1,11 +1,13 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-demo',
-  imports: [],
+  imports: [FormsModule,CommonModule],
   templateUrl: './demo.html',
   styleUrl: './demo.css',
 })
 export class Demo {
-
+  bg:string="hi"
 }
